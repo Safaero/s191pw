f68e7386dd1c5f4416bcb47789dfc572230f85bf
Paul <157764547+Safaero@users.noreply.github.com>
2024-09-27 20:43:22 -0600

    update

--- IntroLaravel/resources/views/formulario.blade.php
+++ IntroLaravel/resources/views/formulario.blade.php
@@ -1,21 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">turista sin maps</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Formulario de registro <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Consultar clientes</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
+@extends('layouts.plantilla1') 
+@section('titulo','Formulario Clientes')
+@section('contenido') 
+
+<div class="container mt-5 col-md-6">
+
+<div class="card font-monospace">
+  <div class="card-header fs-5 text-center text-primary">
+    Registro de Clientes
+ </div>
+ <div class="card-body text-justify">
+  <form>
+    <div class="mb-3">
+      <label for="Nombre" class="form-label">Nombre: </label>
+      <input type="text" class="form-control" id="nombre">
   </div>
-</nav>
-</body>
-</html>
+  <div class="mb-3">
+      <label for="Apellido" class="form-label">Apellido: </label>
+      <input type="text" class="form-control" id="apellido">
+  </div>
+  <div class="mb-3">
+      <label for="correo" class="form-label">Correo: </label>
+      <input type="email" class="form-control" id="correo">
+  </div>
+  <div class="mb-3">
+      <label for="telefono" class="form-label">Telefono: </label>
+      <input type="text" class="form-control" id="telefono">
+  </div>
+
+  <div class="card-footer text-muted">
+    <div class="d-grid gap-2 mt-2 mb-1">
+      <button type="submit" class="btn btn-success btn-sm"> Guardar Cliente</button>
+    </div>
+  </form>
+  </div>
+</div>
+</div>
+@endsection
--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('clientes');
 });
-Route::view('/','welcome');
+Route::view('/','clientes');
 Route::view('/formulario','formulario');
