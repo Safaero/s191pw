49a52d471dd775c8aa7484f0aa31b5fb86626fa6
Paul <157764547+Safaero@users.noreply.github.com>
2024-10-11 20:21:21 -0600

    update11.10.24

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/',[controladorVistas::class,'home'])->name('rutainicio'); 
+
+Route::get('/',[controladorVistas::class,'insert'])->name('rutacacas'); 
+
+Route::get('/',[controladorVistas::class,'select'])->name('rutaconsulta'); 
 
 Route::get('/', function () {
 });
-Route::view('/','inicio')->name('rutainicio');
+/* Route::view('/','inicio')->name('rutainicio');
 Route::view('/consultar','clientes')->name('rutaconsulta');
-Route::view('/form','formulario')->name('rutacacas');
+Route::view('/form','formulario')->name('rutacacas'); */
 Route::view('/component','componentes')->name('rutacomponentes');
